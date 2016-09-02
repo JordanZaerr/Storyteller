02a1590751adbb466fa3e7ee855ae6a546e53865
Jeremy D. Miller <jeremydmiller@yahoo.com>
2016-09-02 12:03:19 -0500

    Thinned down more data shuffling within RemoteController

--- src/ST/CommandLine/ProjectInput.cs
+++ src/ST/CommandLine/ProjectInput.cs
@@ -71,6 +71,11 @@ namespace ST.CommandLine
             var path = Path.ToFullPath();
             var project = Project.LoadForFolder(path);
 
+            if (ConfigFlag.IsNotEmpty())
+            {
+                project.ConfigFile = ConfigFlag;
+            }
+
             var controller = new RemoteController(project);
             controller.Project.Culture = CultureFlag;
 
@@ -103,10 +108,7 @@ namespace ST.CommandLine
             controller.Project.MaxRetries = RetriesFlag;
             controller.Project.Profile = ProfileFlag;
 
-            if (ConfigFlag.IsNotEmpty())
-            {
-                controller.ConfigFile = ConfigFlag;
-            }
+
 
             return controller;
         }
